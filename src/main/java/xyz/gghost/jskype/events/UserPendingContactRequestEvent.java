89a0f591d371a51d525d98efe5ed3dcec69ba131
Blinky™ <d@nkmem.es>
2015-10-02 07:52:30 -0600

    Update UserPendingContactRequestEvent.java

--- src/main/java/xyz/gghost/jskype/events/UserPendingContactRequestEvent.java
+++ src/main/java/xyz/gghost/jskype/events/UserPendingContactRequestEvent.java
@@ -2,11 +2,17 @@ package xyz.gghost.jskype.events;
 
 import lombok.Getter;
 import xyz.gghost.jskype.event.Event;
+import xyz.gghost.jskype.SkypeAPI;
+import xyz.gghost.jskype.internal.packet.packets.GetPendingContactsPacket;
 
 @Getter
 public class UserPendingContactRequestEvent extends Event {
     private String user;
     public UserPendingContactRequestEvent(String user){
         this.user = user;
+    }
+
+    public void accept(SkypeAPI skype) {
+        new GetPendingContactsPacket(skype).acceptRequest(user);
     }
 }
