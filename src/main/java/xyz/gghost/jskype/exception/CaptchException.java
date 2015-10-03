2ad463ec485ea76960330681c53b24d211f1dde7
Joe Burnard <spongybacon@hotmail.co.uk>
2015-10-03 17:46:22 +0100

    Correct typo

--- src/main/java/xyz/gghost/jskype/exception/CaptchException.java
+++ src/main/java/xyz/gghost/jskype/exception/CaptchException.java
@@ -3,5 +3,5 @@ package xyz.gghost.jskype.exception;
 /**
  * Created by Ghost on 19/09/2015.
  */
-public class CaptchException extends Exception {
+public class CaptchaException extends Exception {
 }
