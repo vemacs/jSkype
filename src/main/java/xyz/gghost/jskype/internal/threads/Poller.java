6cc492c5dcd8c40367cdd2cf2fdcc0c35a3feadb
Ghost <ghost@ghosted.me>
2015-10-17 18:24:20 +0100

    Update Poller.java

--- src/main/java/xyz/gghost/jskype/internal/threads/Poller.java
+++ src/main/java/xyz/gghost/jskype/internal/threads/Poller.java
@@ -19,8 +19,6 @@ import java.util.List;
 public class Poller extends Thread {
 
     private SkypeAPI api;
-    private List<Thread> threads = new ArrayList<Thread>();
-    private boolean breakLoop = false;
     public List<PollRequest> processors = new ArrayList<PollRequest>();
     public ArrayList<Integer> pastIds = new ArrayList<Integer>();
     public Poller(SkypeAPI api) {
@@ -39,87 +37,77 @@ public class Poller extends Thread {
             add(new PingPoll(api));
         }});
 
-        while (this.isAlive() || breakLoop) {
-            Thread processingThread = new Thread() {
-                public void run() {
-                    poll(this);
-                }
-            };
-            threads.add(processingThread);
-            processingThread.start();
-
-            try{
-                sleep(200);
-            }catch (Exception e){}
+        while(true) {
+            poll(this);
         }
+
     }
 
     public void stopThreads(){
-        breakLoop = true;
-        for (Thread thread : threads)
-            thread.interrupt();
+
     }
 
     private void poll(Thread h) {
-        PacketBuilder poll = new PacketBuilder(api);
-        poll.setType(RequestType.POST);
-        poll.setUrl("https://client-s.gateway.messenger.live.com/v1/users/ME/endpoints/SELF/subscriptions/0/poll");
-        poll.setData(" ");
+        try {
+            PacketBuilder poll = new PacketBuilder(api);
+            poll.setType(RequestType.POST);
+            poll.setUrl("https://client-s.gateway.messenger.live.com/v1/users/ME/endpoints/SELF/subscriptions/0/poll");
+            poll.setData(" ");
 
-        String data = poll.makeRequest();
+            String data = poll.makeRequest();
 
-        if (data == null || data.equals("") || data.equals("{}"))
-            return;
+            if (data == null || data.equals("") || data.equals("{}"))
+                return;
 
-        JSONArray json = new JSONObject(data).getJSONArray("eventMessages");
-        for (int i = 0; i < json.length(); i++) {
-            JSONObject object = json.getJSONObject(i);
+            JSONArray json = new JSONObject(data).getJSONArray("eventMessages");
+            for (int i = 0; i < json.length(); i++) {
+                JSONObject object = json.getJSONObject(i);
 
-            if (!(object.isNull("type") && object.isNull("resourceType"))) {
+                if (!(object.isNull("type") && object.isNull("resourceType"))) {
 
-                String resourceLink = object.isNull("resourceLink") ? "" : object.getString("resourceLink");
-                JSONObject resource = object.getJSONObject("resource");
+                    String resourceLink = object.isNull("resourceLink") ? "" : object.getString("resourceLink");
+                    JSONObject resource = object.getJSONObject("resource");
 
-                if  (resource.isNull("resourceLink"))
-                    resource.put("resourceLink", resourceLink);
+                    if (resource.isNull("resourceLink"))
+                        resource.put("resourceLink", resourceLink);
 
-                if (api.isDebugMode())
-                    System.out.println(object);
+                    if (api.isDebugMode())
+                        System.out.println(object);
 
-                Group chat = null;
+                    Group chat = null;
 
-                if (resourceLink.contains("conversations/19:") || resourceLink.contains("8:")) {
-                    if (!resourceLink.contains("8:")) {
-                        addGroupToRecent(object);
-                        chat = api.getGroupById(NamingUtils.getThreadId(resourceLink));
-                    } else if (!resourceLink.contains("@")) {
-                        chat = new ContactGroupImpl(api, "8:" + NamingUtils.getUsername(resourceLink));
+                    if (resourceLink.contains("conversations/19:") || resourceLink.contains("8:")) {
+                        if (!resourceLink.contains("8:")) {
+                            addGroupToRecent(object);
+                            chat = api.getGroupById(NamingUtils.getThreadId(resourceLink));
+                        } else if (!resourceLink.contains("@")) {
+                            chat = new ContactGroupImpl(api, "8:" + NamingUtils.getUsername(resourceLink));
+                        }
                     }
-                }
 
 
-                if ((!object.isNull("id")) && (pastIds.contains(object.getInt("id")))) {
-                    if(api.isDebugMode())
-                        api.log("Warning! Skype might be behind us!");
-                    continue;
-                }
-
-                for (PollRequest pollProcessor : processors) {
-                    try {
-                        if (pollProcessor.isMe(object))
-                            pollProcessor.process(resource, chat);
-                    } catch (Exception e) {
-                        api.log("Failed to process data from skype.\nMessagePoll: " + object + "Data: " + data + "\nError: " + e.getMessage());
-                        api.log("\n\nIs this a new convo?\nWait a few seconds!");
-                        e.printStackTrace();
+                    if ((!object.isNull("id")) && (pastIds.contains(object.getInt("id")))) {
+                        if (api.isDebugMode())
+                            api.log("Warning! Skype might be behind us!");
+                        continue;
                     }
-                }
 
-                if (!object.isNull("id"))
-                    pastIds.add(object.getInt("id"));
+                    for (PollRequest pollProcessor : processors) {
+                        try {
+                            if (pollProcessor.isMe(object))
+                                pollProcessor.process(resource, chat);
+                        } catch (Exception e) {
+                            api.log("Failed to process data from skype.\nMessagePoll: " + object + "Data: " + data + "\nError: " + e.getMessage());
+                            api.log("\n\nIs this a new convo?\nWait a few seconds!");
+                            e.printStackTrace();
+                        }
+                    }
+
+                    if (!object.isNull("id"))
+                        pastIds.add(object.getInt("id"));
+                }
             }
-        }
-        threads.remove(h);
+        }catch(Exception e){}
     }
 
 
